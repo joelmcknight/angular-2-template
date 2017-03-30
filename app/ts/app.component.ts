82e79a31b45046a7eecb1bdfb69c4d26fe34322a
Joel McKnight <joemckni@catorpww6562.global.publicisgroupe.net>
2017-03-30 15:52:31 -0400

    second commit

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,28 @@
 import {Component} from 'angular2/core';
+import {Config} from './config.service';
+import {Video} from './video';
+import {VideoPlayerComponent} from "./videoPlayer.component";
+import {PlaylistComponent} from'./playlist.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponent, VideoPlayerComponent]
 })
 
-export class AppComponent {}
+export class AppComponent {
+	mainHeading = Config.MAIN_HEADING;
+	videos:Array<Video>;
+
+	constructor(){
+		this.videos = [
+		new Video(1, "angular tutorial 7","f8qBeaGe2S4", "how to create nested components"),
+		new Video(2, "black cab", "YfZPz7okidc", "chinese rap")
+		];
+		  this.video = this.videos[0];
+	}
+	 onVideoSelected(video: Video) {
+        this.video = video;
+    }
+
+}
---
+++ app/ts/playlist.component.ts
@@ -0,0 +1,28 @@
+import {Component, Input, Output, EventEmitter} from 'angular2/core';
+import {Video} from './video';
+
+@Component({
+	selector: 'playlist',
+	templateUrl: 'app/ts/playlist.component.html',
+	inputs: ['videos']
+})
+
+export class PlaylistComponent{
+
+	 @Input() videos: Array<Video>;
+    @Output() onSelectedVideo = new EventEmitter<Video>();
+    currentVideo: Video;
+
+    // Not really necessary but I don't want it to be null at the beginning so I set it to the first one in the list
+    ngOnInit() {
+        this.currentVideo = this.videos[0];
+    }
+
+	onSelect(vid:Video){
+		console.log(JSON.stringify(vid));
+		// this.onSelectedVideo.emit(video);
+        // this.currentVideo = video;
+
+	}
+
+}
